f771f5ec555f86e9410273647c20acbd32ea4e8e
mpuntus-css <mpuntus@css.edu>
2025-11-16 23:46:27 -0600

    Basic version of the UI for the app

---
+++ lib/models/category.dart
@@ -0,0 +1,7 @@
+class CategoryModel {
+  final String name;
+  final String icon;
+
+
+  CategoryModel({required this.name, required this.icon});
+}
---
+++ lib/models/expense.dart
@@ -0,0 +1,9 @@
+class Expense {
+  final double amount;
+  final String category;
+  final DateTime date;
+  final String note;
+
+
+  Expense({required this.amount, required this.category, required this.date, required this.note});
+}
